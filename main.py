d244089b39271562613255998594ba81d208a15e
h4fide <40579109+h4fide@users.noreply.github.com>
2022-08-20 19:38:17 +0100

    .

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def main():
 if __name__ == "__main__":
     fileslist = os.listdir('src/')
     requires_files = ["bot.py", "patrol.py"]
-    print(requires_files)
     for file in requires_files:
         if file not in fileslist:
             print(f"{file} is missing")
